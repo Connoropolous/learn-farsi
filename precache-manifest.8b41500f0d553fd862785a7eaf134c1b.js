4f30ea80e35edd0a96834dab63fb6a52ce678e9b
Connor Turland <connorturland@gmail.com>
2020-05-04 09:30:35 -0400

    Updates

---
+++ precache-manifest.8b41500f0d553fd862785a7eaf134c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173084b6d4286e7e38424cccd02767fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b22c0d62c7a9bac153d",
+    "url": "/static/css/main.0a833b62.chunk.css"
+  },
+  {
+    "revision": "b3e243b70fe5864006c2",
+    "url": "/static/js/2.81ac3177.chunk.js"
+  },
+  {
+    "revision": "dde2f98dd0ed8bfad18d88defdcf5f43",
+    "url": "/static/js/2.81ac3177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b22c0d62c7a9bac153d",
+    "url": "/static/js/main.14016fd1.chunk.js"
+  },
+  {
+    "revision": "9c9b3298452846bb5dc2",
+    "url": "/static/js/runtime-main.8fd9eff9.js"
+  }
+]);
