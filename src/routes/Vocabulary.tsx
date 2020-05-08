28f50d6cee935e99a6081b77f18375cfe3646eaa
Connor Turland <connorturland@gmail.com>
2020-05-08 12:50:43 -0400

    add links to vocabulary

--- src/routes/Vocabulary.tsx
+++ src/routes/Vocabulary.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
 export default function Vocabulary() {
-  return <div>hi vocabulary</div>
+  return (
+    <div>
+      <a
+        href='https://github.com/Connoropolous/farsi-data/blob/master/verbs.csv'
+        target='_blank'
+      >
+        verbs
+      </a>
+      <br />
+      <a
+        href='https://github.com/Connoropolous/farsi-data/blob/master/nouns.csv'
+        target='_blank'
+      >
+        nouns
+      </a>
+    </div>
+  )
 }
